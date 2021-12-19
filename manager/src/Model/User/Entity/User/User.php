63e9d8f6ea65fe4b85242b7ff3beb5664c55f706
eaglezzzzz <eaglecrazy@mail.ru>
2021-12-19 15:31:54 +0300

    Обновил \App\Model\User\Entity\User\User

--- manager/src/Model/User/Entity/User/User.php
+++ manager/src/Model/User/Entity/User/User.php
@@ -6,7 +6,35 @@ namespace App\Model\User\Entity\User;
 
 class User
 {
-    public function __construct(string $email, string $password)
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $passwordHash;
+
+    public function __construct(string $email, string $hash)
     {
+        $this->email        = $email;
+        $this->passwordHash = $hash;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPasswordHash(): string
+    {
+        return $this->passwordHash;
     }
 }
