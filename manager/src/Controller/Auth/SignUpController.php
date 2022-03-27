e3776a0b7a41176dfe76682bfdef41ef05e81daf
eaglezzzzz <eaglecrazy@mail.ru>
2022-03-27 16:10:15 +0300

    Добавил подтверждение email.

--- manager/src/Controller/Auth/SignUpController.php
+++ manager/src/Controller/Auth/SignUpController.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace App\Controller\Auth;
 
 use App\Model\User\UseCase\SignUp;
-use App\Model\User\UseCase\SignUp\Request\Command;
+use App\Model\User\UseCase\SignUp\Confirm\Command as ConfirmCommand;
+use App\Model\User\UseCase\SignUp\Request\Command as RequestCommand;
 use App\Model\User\UseCase\SignUp\Request\Form;
-use App\Model\User\UseCase\SignUp\Request\Handler;
+use App\Model\User\UseCase\SignUp\Request\Handler as RequestHandler;
+use App\Model\User\UseCase\SignUp\Confirm\Handler as ConfirmHandler;
 use DomainException;
+use phpDocumentor\Reflection\Types\This;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,12 +30,12 @@ class SignUpController extends AbstractController
     /**
      * @Route("/signup", name="auth.signup")
      * @param Request $request
-     * @param Handler $handler
+     * @param RequestHandler $handler
      * @return Response
      */
-    public function request(Request $request, Handler $handler): Response
+    public function request(Request $request, RequestHandler $handler): Response
     {
-        $command = new Command();
+        $command = new RequestCommand();
 
         $form = $this->createForm(Form::class, $command);
         $form->handleRequest($request);
@@ -49,8 +52,28 @@ class SignUpController extends AbstractController
             }
         }
 
-        return $this->render('app/auth/signup', [
+        return $this->render('app/auth/signup.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    /**
+     * @route("/signup/{token}", name="auth.signup.confirm")
+     * @param string $token
+     * @param ConfirmHandler $handler
+     * @return Response
+     */
+    public function comfirm(string $token, ConfirmHandler $handler): Response {
+        $command = new ConfirmCommand($token);
+
+        try {
+            $handler->handle($command);
+            $this->addFlash('success', 'Email подтверждён успешно.');
+            return $this->redirectToRoute('home');
+        } catch (DomainException $e) {
+            $this->logger->error($e->getMessage(), ['exception' => $e]);
+            $this->addFlash('error', $e->getMessage());
+            return $this->redirectToRoute('home');
+        }
     }
 }
--- manager/src/Model/User/Service/ConfirmTokenSender.php
+++ manager/src/Model/User/Service/ConfirmTokenSender.php
@@ -3,8 +3,51 @@
 namespace App\Model\User\Service;
 
 use App\Model\User\Entity\User\Email;
+use RuntimeException;
+use Swift_Mailer;
+use Swift_Message;
+use Twig\Environment;
 
-interface ConfirmTokenSender
+class ConfirmTokenSender
 {
-    public function send(Email $email, string $token): void;
+    /**
+     * @var Swift_Mailer
+     */
+    private $mailer;
+
+    /**
+     * @var Environment
+     */
+    private $twig;
+
+    /**
+     * @var array
+     */
+    private $form;
+
+    /**
+     * @param Swift_Mailer $mailer
+     * @param Environment $twig
+     * @param array $from
+     */
+    public function __construct(Swift_Mailer $mailer, Environment $twig, array $from)
+    {
+        $this->mailer = $mailer;
+        $this->twig   = $twig;
+        $this->form   = $from;
+    }
+
+    public function send(Email $email, string $token): void
+    {
+        $message = (new Swift_Message('Подтверждение email'))
+            ->setFrom($this->form)
+            ->setTo($email->getValue())
+            ->setBody($this->twig->render('mail/user/signup.html.twig', [
+                $token => $token,
+            ]), 'text/html');
+
+        if(!$this->mailer->send($message)) {
+            throw new RuntimeException('Unable to send message.');
+        }
+    }
 }
