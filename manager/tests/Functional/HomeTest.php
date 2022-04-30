f11b34623fb203250b8112d6b4e4500e0b0181ea
eaglezzzzz <eaglecrazy@mail.ru>
2022-04-30 20:24:43 +0300

    Добавил функциональное тестирование.

---
+++ manager/tests/Functional/HomeTest.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomeTest extends WebTestCase
+{
+    public function testGuest()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertSame(302, $client->getResponse()->getStatusCode());
+        $this->assertSame('http://localhost/login', $client->getResponse()->headers->get('Location'));
+    }
+
+    public function testSuccess()
+    {
+        $client = static::createClient([], [
+                'PHP_AUTH_USER' => 'admin@app.test',
+                'PHP_AUTH_PW'   => 'password',
+            ]
+        );
+
+        $crawler = $client->request('GET', '/');
+
+        $client->request('GET', '/');
+
+        $this->assertSame(200, $client->getResponse()->getStatusCode());
+        $this->assertSame('Hello world!', $crawler->filter('h1')->text());
+    }
+}
